da4b8a26d6682a4e436f7a937a2fc19159a81b85
Jose Oronia <jaoronia@gmail.com>
2021-08-09 00:43:21 -0700

    Check to see if the month and year already exists in file
    Code to not readd the current month being recalculated

--- monthlySpending.py
+++ monthlySpending.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
+import os
 
 def writeDataToCSV():
     col_names = ['Date','Description','Debit','Credit','Category']
-    df = pd.read_csv("../Personal_Finance/August_expense.csv", names=col_names,skiprows=6).drop(columns='Credit')
+    df = pd.read_csv("../Personal_Finance/Last_Month_July.csv", names=col_names,skiprows=6).drop(columns='Credit')
+    df1 = None
+    df2 = None
 
     sum_of_categories = df.groupby(['Category'])['Debit'].sum().reset_index(name = "Total Amount")
 
@@ -24,19 +27,37 @@ def writeDataToCSV():
 
     amount_spent_in_month_df = pd.DataFrame(data=amount_spent_in_month)
 
-    with open('../Personal_Finance/category_to_date.csv', 'a') as f:
-      f.write('\n') 
-      sum_of_categories.to_csv('../Personal_Finance/category_to_date.csv',mode='a', header=False)
+    if os.stat("../Personal_Finance/category_to_date.csv").st_size > 0:
+      df1 = pd.read_csv("../Personal_Finance/category_to_date.csv",names=["index","Category","Total Amount","Spent","Percentage","Month_year"])
+      monthYearList = df1['Month_year'].tolist()
+      if sum_of_categories['Month_year'].iloc[0] not in monthYearList:
+        with open('../Personal_Finance/category_to_date.csv', 'a') as f:
+          f.write('\n') 
+          sum_of_categories.to_csv('../Personal_Finance/category_to_date.csv',mode='a', header=False)
+      df1 = pd.read_csv("../Personal_Finance/category_to_date.csv",names=["index","Category","Total Amount","Spent","Percentage","Month_year"]) 
+    else:
+       with open('../Personal_Finance/category_to_date.csv', 'a') as f:
+          f.write('\n') 
+          sum_of_categories.to_csv('../Personal_Finance/category_to_date.csv',mode='a', header=False)
+          df1 = pd.read_csv("../Personal_Finance/category_to_date.csv",names=["index","Category","Total Amount","Spent","Percentage","Month_year"])
+    
+    if os.stat("../Personal_Finance/spent_by_month.csv").st_size > 0:
+      df2 = pd.read_csv("../Personal_Finance/spent_by_month.csv",names=["amount","Month_Year"])
+      monthYearList = df2['Month_Year'].tolist()
+      if sum_of_categories['Month_year'].iloc[0] not in monthYearList:
+        with open('../Personal_Finance/spent_by_month.csv', 'a') as f:
+          f.write('\n') 
+          amount_spent_in_month_df.to_csv('../Personal_Finance/spent_by_month.csv',mode='a', header=False)
+      df2 = pd.read_csv("../Personal_Finance/spent_by_month.csv",names=["amount","Month_Year"]) # We need to reread the file for new changes
+    else:
+       with open('../Personal_Finance/spent_by_month.csv', 'a') as f:
+          f.write('\n') 
+          amount_spent_in_month_df.to_csv('../Personal_Finance/spent_by_month.csv',mode='a', header=False)
+          df2 = pd.read_csv("../Personal_Finance/spent_by_month.csv",names=["amount","Month_Year"])
 
-    with open('../Personal_Finance/spent_by_month.csv', 'a') as f:
-     f.write('\n') 
-     amount_spent_in_month_df.to_csv('../Personal_Finance/spent_by_month.csv',mode='a', header=False)
+    showGraphs(labels,percentage,df1,df2)
 
-    showGraphs(labels,percentage)
-
-def showGraphs(labels,percentage):
-    df1 = pd.read_csv("../Personal_Finance/category_to_date.csv",names=["index","Category","Total Amount","Spent","Percentage","Month_year"])
-    df2 = pd.read_csv("../Personal_Finance/spent_by_month.csv",names=["amount","Month_Year"])
+def showGraphs(labels,percentage,df1,df2):
 
     energy = df2['amount'].tolist()
     plt.bar(df2["Month_Year"].tolist(), energy, color='green')
